9b700240deb42ccaeaf6ef3dfe634cf64969c499
MateuszFalkowski01 <s27426@pjwstk.edu.pl>
2023-06-04 10:29:52 +0200

    Aktualizacja

--- Zadania2/src/Zadanie4/Zadanie4.java
+++ Zadania2/src/Zadanie4/Zadanie4.java
@@ -1,14 +1,9 @@
+package Zadanie4;
 public class Zadanie4 {
     public static void
     main(String[] args) {
         String str1 = "napis1";
         String str2 = "napis2";
-        boolean b = false;
-        if (str1==str2){
-            b=true;
-        }
-        System.out.println(b);
-
-
+        System.out.println(str1.equals(str2));
     }
 }
